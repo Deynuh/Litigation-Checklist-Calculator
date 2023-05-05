30778b3d3b41c04fda10c1f4c4cf092bcec1652d
Deynuh <60407390+Deynuh@users.noreply.github.com>
2023-05-05 05:08:03 -0700

    working on css

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,21 @@ function App() {
           Litigation Checklist Calculator
         </h1>
       </div>
+
+      <div className="containers">
+        <div className="NOCC">
+          <h2> Notice of Civil Claim</h2>
+        </div>
+
+        <div className="CC">
+          <h2> Counterclaim</h2>
+        </div>
+
+        <div className="RTCC">
+          <h2> Response to Civil Claim</h2>
+        </div>
+      </div>
+      
     </div>
   )
 }
