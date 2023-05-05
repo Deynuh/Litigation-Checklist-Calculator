22d9b26039581927d6bc14b1ae5c447d25e779c4
antho <anthonyjrchen@gmail.com>
2023-05-05 07:33:29 -0700

    started function that updates table

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -1,11 +1,12 @@
 import React, { useRef, useState } from 'react';
-
+import EventEmitter from '../utils/EventEmitter';
 const DatePicker = () => {
   const [date, setDate] = useState('');
   const dateInputRef = useRef(null);
 
   const handleChange = (e) => {
     setDate(e.target.value);
+    EventEmitter.emit('selectedDate',e.target.value);
   };
 
   return (
--- src/components/NOCC_Table.jsx
+++ src/components/NOCC_Table.jsx
@@ -6,21 +6,141 @@ import TableContainer from '@mui/material/TableContainer';
 import TableHead from '@mui/material/TableHead';
 import TableRow from '@mui/material/TableRow';
 import Paper from '@mui/material/Paper';
-
+import {useState,useEffect} from 'react';
+import EventEmitter from '../utils/EventEmitter';
 function createData(file, date) {
   return {file, date};
 }
 
-const rows = [
-  createData('Jurisdictional Response', '20 days'),
-  createData('Response to Civil Claim', '21 days'),
-  createData('Counterclaim', '21 days'),
-  createData('NOCC and Counterclaim New Party', '60 days'),
-  createData('Third Party Notice', '42 days [1]'),
 
-];
+
 
 export default function BasicTable() {
+  let[test,setTest] = useState("No Date Selected");
+  var jrDate,rccDate,ccDate,noccDate,tpnDate;
+  var rows = [
+    createData('Jurisdictional Response', test),
+    createData('Response to Civil Claim', rccDate),
+    createData('Counterclaim', ccDate),
+    createData('NOCC and Counterclaim New Party', noccDate),
+    createData('Third Party Notice', tpnDate),
+
+  ];
+  
+  useEffect(()=>{
+    const setDates = (eventData)=>{
+      var d = 14, m = 3, y = 2015;
+      var x = 366;
+      setTest(addDays(d, m, y, x));
+      
+      console.log("date was selected:" + addDays(d, m, y, x));
+    }
+    const listener = EventEmitter.addListener('selectedDate',setDates);
+    return ()=> {
+      listener.remove();
+    }
+  })
+
+  var m2, d2;
+
+  function isLeap(y)
+  {
+      if (y % 100 != 0 &&
+          y % 4 == 0 ||
+          y % 400 == 0)
+          return true;
+   
+      return false;
+  }
+  
+  function offsetDays(d , m , y)
+  {
+      var offset = d;
+   
+      if (m - 1 == 11)
+          offset += 335;
+      if (m - 1 == 10)
+          offset += 304;
+      if (m - 1 == 9)
+          offset += 273;
+      if (m - 1 == 8)
+          offset += 243;
+      if (m - 1 == 7)
+          offset += 212;
+      if (m - 1 == 6)
+          offset += 181;
+      if (m - 1 == 5)
+          offset += 151;
+      if (m - 1 == 4)
+          offset += 120;
+      if (m - 1 == 3)
+          offset += 90;
+      if (m - 1 == 2)
+          offset += 59;
+      if (m - 1 == 1)
+          offset += 31;
+   
+      if (isLeap(y) && m > 2)
+          offset += 1;
+   
+      return offset;
+  }
+   
+  
+  function revoffsetDays(offset, y)
+  {
+      var month = [ 0, 31, 28, 31, 30, 31, 30,
+                    31, 31, 30, 31, 30, 31 ];
+   
+      if (isLeap(y))
+          month[2] = 29;
+   
+      var i;
+      for(i = 1; i <= 12; i++)
+      {
+          if (offset <= month[i])
+              break;
+               
+          offset = offset - month[i];
+      }
+      d2 = offset;
+      m2 = i;
+  }
+   
+  
+  function addDays(d1 , m1 , y1 , x)
+  {
+      var offset1 = offsetDays(d1, m1, y1);
+      var remDays = isLeap(y1) ? (366 - offset1) :
+                                 (365 - offset1);
+   
+   
+      var y2, offset2 = 0;
+      if (x <= remDays)
+      {
+          y2 = y1;
+          offset2 = offset1 + x;
+      }
+      else
+      {
+  
+          x -= remDays;
+          y2 = y1 + 1;
+          var y2days = isLeap(y2) ? 366 : 365;
+           
+          while (x >= y2days)
+          {
+              x -= y2days;
+              y2++;
+              y2days = isLeap(y2) ? 366 : 365;
+          }
+          offset2 = x;
+      }
+       
+      revoffsetDays(offset2, y2);
+      var value = d2 + "/" + m2 + "/" + y2;
+      return value;
+  }
   return (
     <TableContainer component={Paper}>
       <Table sx={{ minWidth: 100 }} aria-label="simple table">
